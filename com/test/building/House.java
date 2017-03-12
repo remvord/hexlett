834ce69d839ab65e9a2b465df29fd31f3667b7ed
remvord <rem-den@yandex.ru>
2017-03-12 21:41:07 +0300

    user input

---
+++ com/test/building/House.java
@@ -0,0 +1,10 @@
+package com.test.buildings;
+import com.test.humans.Human;
+
+public class House {
+
+	String address;
+	
+	Human owner;
+
+}
---
+++ com/test/humans/Human.java
@@ -0,0 +1,16 @@
+package com.test.humans;
+
+public class Human {
+
+	String name;
+
+	public static void main(String[] args) {
+			System.out.println("Hello!");
+		
+	}
+
+}
+
+
+
+
